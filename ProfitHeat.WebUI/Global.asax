5ded40e10125ec1c79aada55e1da4907ec652268
Alexey-O-Melnikov <Alexey.O.Melnikov@yandex.ru>
2016-04-21 09:10:28 +0300

    Добавьте файлы проекта.

---
+++ ProfitHeat.WebUI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ProfitHeat.WebUI.MvcApplication" Language="C#" %>
---
+++ ProfitHeat.WebUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProfitHeat.WebUI.Startup))]
+namespace ProfitHeat.WebUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
